3e0b12d0679348db4b2002c7f0018d539256c24c
Angret Kyi <shwekyiangret1512@gmail.com>
2022-02-02 01:29:17 +0630

    my profile webiste

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const imgOne = "images/background1.png";
+const imgTwo = "images/background8.jpg";
+const imgThree = "images/background7.png";
+const imgFour = "images/background4.png";
+const imgFive = "images/background10.png";
+const imgSix = "images/background2.png";
+const imgSeven = "images/background6.png";
+const imgEight = "images/background9.png";
+const imgNine = "images/background3.png";
+const imgTen = "images/background5.png";
+
+var images = [
+  imgOne,
+  imgTwo,
+  imgThree,
+  imgFour,
+  imgFive,
+  imgSix,
+  imgSeven,
+  imgEight,
+  imgNine,
+  imgTen,
+];
+
+var slider = setInterval(function () {
+  document
+    .getElementsByClassName("top-container")[0]
+    .setAttribute("style", 'background-image: url("' + images[0] + '")');
+  images.splice(images.length, 0, images[0]);
+  images.splice(0, 1);
+}, 3000);
+
+$(function () {
+  // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse("hide");
+    }
+  });
+});
